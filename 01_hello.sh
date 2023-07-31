a74aa6ba0a7c8b5bfbbf07fd2edb351f0c763df5
kapil-malviya <etc.kapil@gmail.com>
2023-07-31 18:21:59 +0530

    starting with basics

---
+++ 01_hello.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "hello folks..!"
+
+echo our shell name is $BASH
+echo our shell version is $BASH_VERSION
+echo our home directory is $HOME
+echo our present working directory is $PWD
+
+name=lord               # a var name can not starts with an integer or number
+
+echo the name is $name
